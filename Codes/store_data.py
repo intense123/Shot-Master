4ef37afc408edeeb3ccb07d62dd352842d893d8e
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-05-05 01:28:50 +0600

    Two files merged

--- Codes/store_data.py
+++ Codes/store_data.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+import csv
+import os
+
+csv_file_path = os.path.join(os.getcwd(), 'F:\\ShotMaster\\codes\\shots.csv')
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/sensor_data'
@@ -19,6 +23,19 @@ def receive_data():
     mongo.db.sensor_data.insert_one(data)
     return jsonify({"message": "Data received and stored successfully"})
 
+@app.route('/create_db_from_csv')
+def create_db_from_csv():
+    # Drop existing database if it exists
+    mongo.db.client.drop_database('myDatabase')
+    
+    # Read CSV file and insert data into MongoDB
+    with open(csv_file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # Insert each row into MongoDB
+            mongo.db.shots.insert_one(row)
+    return 'Data inserted into MongoDB from CSV successfully!'
+
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
@@ -35,5 +52,5 @@ def handle_sensor_data(data):
     emit('sensor_data', data, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
     socketio.run(app, debug=True)
