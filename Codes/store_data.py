1e45269f7596a8bf4db69c851ecb6755bd331060
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-05-04 11:09:12 +0600

    Receive sensor data

--- Codes/store_data.py
+++ Codes/store_data.py
@@ -1,14 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
-from pymongo import MongoClient
+from pymongo import MongoClient, PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/sensor_data'
+mongo = PyMongo(app)
 socketio = SocketIO(app)
 
 # MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
 db = client['sensor_data']
 collection = db['data']
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.json
+    mongo.db.sensor_data.insert_one(data)
+    return jsonify({"message": "Data received and stored successfully"})
 
 @socketio.on('connect')
 def handle_connect():
@@ -26,4 +34,5 @@ def handle_sensor_data(data):
     emit('sensor_data', data, broadcast=True)
 
 if __name__ == '__main__':
+    app.run(debug=True)
     socketio.run(app, debug=True)
